ddf57de88cbac2618029a8678322dc2f2eb746c6
Іван Попович <ivanpopovic@Ivans-MacBook-Pro.local>
2020-03-14 21:01:19 +0200

    master

--- js/load-json.js
+++ js/load-json.js
@@ -60,6 +60,22 @@ var show_catalog = {
 		});
 	}
 }
+var status_modal = {
+	fun_er: function(th){
+		$('.modal-success').removeClass('active');
+		$('.modal-error[data-stasus="' + th + '"]').addClass('active');
+		setTimeout(function(){
+			$('.modal-error').removeClass('active');
+		}, 2000);
+	},
+	fun_success: function(th){
+		$('.modal-error').removeClass('active');
+		$('.modal-success[data-stasus="' + th + '"]').addClass('active'); 
+		setTimeout(function(){
+			$('.modal-success').removeClass('active');
+		}, 2000);
+	}
+}
 /*filter*/
 var fillter_catalog = {
 	fun: function(name_json){
@@ -86,26 +102,18 @@ var fillter_catalog = {
 		});
 		$('.fill_submit input').click(function(event) {
 			event.preventDefault();
-
-			
-
-			
-
-
 			var result_fill = new Object();
 			$(this).parents('form').find('label[data-value]').each(function(index, el) {
 				var th_attr = $(this).attr('data-value');
 				var th_val = $(this).find('input, select').val();
 				if(th_val != "all" && th_val != "") result_fill[th_attr] = th_val
 			});
-
-
 			function fun_check(){
 				var checked = $('input[data-city="off"]').prop('checked');
 				if(checked == true) result_fill.work_online =  true
 			}
 			fun_check();
-			var remover_id = new Array();
+			var remove_id = new Array();
 			for (const prop in result_fill) {
 				for (var i = 0; i < name_json.length; i++) {
 					if(name_json[i].filter[prop] != result_fill[prop]
@@ -115,17 +123,17 @@ var fillter_catalog = {
 						&& prop != "experience"
 						&& prop != "equipment")
 					{
-						remover_id.push(name_json[i].id);
+						remove_id.push(name_json[i].id);
 					}
 					// key-poblem
 					if(prop == "key-poblem"){
 						if(name_json[i].filter[prop].search(result_fill[prop]) != -1 == false){
-							remover_id.push(name_json[i].id);
+							remove_id.push(name_json[i].id);
 						}
 					}
 					if(prop == "equipment"){
 						if(name_json[i].filter[prop].search(result_fill[prop]) != -1 == false){
-							remover_id.push(name_json[i].id);
+							remove_id.push(name_json[i].id);
 						}
 					}
 					// min price / max price
@@ -134,19 +142,19 @@ var fillter_catalog = {
 						var max =+ result_fill.max_price;
 						if(min == undefined) min = 0;
 						if(max == undefined) max = Infinity;
-						if(name_json[i].filter.price < min || name_json[i].filter.price > max) remover_id.push(name_json[i].id);
+						if(name_json[i].filter.price < min || name_json[i].filter.price > max) remove_id.push(name_json[i].id);
 					}
 					// experience
 					if(prop == "experience"){
 						var exp_user =+ name_json[i].filter.experience;
 						var ext_select =+ result_fill[prop];
-						if(exp_user < ext_select) remover_id.push(name_json[i].id);
+						if(exp_user < ext_select) remove_id.push(name_json[i].id);
 					}
 					// select online
 					if(prop == "work_online"){
 						var status = name_json[i].filter.work_online;
 						if(status == false){
-							remover_id.push(name_json[i].id);
+							remove_id.push(name_json[i].id);
 						}
 					}
 				}
@@ -159,7 +167,7 @@ var fillter_catalog = {
 			$('.items-catalog > .hide > .item').unwrap('.hide');
 			$('.items-catalog > .item').each(function(index, el) {
 				var th_index = $(this).index();
-				var result = remover_id.unique().indexOf(th_index + 1);
+				var result = remove_id.unique().indexOf(th_index + 1);
 				if(result >= 0){
 					$(this).wrap('<div class="hide"></div>');
 				}
@@ -169,9 +177,118 @@ var fillter_catalog = {
 		});
 	}
 }
+/*raiting*/
+var reiting_fun = {
+	fun: function(json){
+		// output to data-reiting
+		for (var i = 0; i < json.length; i++) {
+			var formula = 0;
+			var count_user = 0;
+			for (var key in json[i].reiting){
+				count_user = count_user + json[i].reiting[key];
+				var reiting =+ key;
+				formula = reiting * json[i].reiting[key] + formula;
+			}
+			var results = formula/count_user;
+			results = results.toFixed(2)
+			$('.items-catalog .item').eq(i).find('.ul-reiting').attr('data-reiting', results);
+		}
+		// display in the graph
+		function each_reiting(th){
+			th.find('li').removeClass('active-whole disabled h_25 h_5 h_75')
+			th.each(function(index, el) {
+				var reiting = $(this).attr('data-reiting');
+				if(reiting != "NaN"){
+					reiting =+ reiting;
+					var whole_values = Math.floor(reiting);
+					var small_value = reiting - whole_values;
+					small_value =+ small_value.toFixed(2);
+					$(this).find('li').eq(whole_values - 1).addClass('active-whole').nextAll('li').addClass('disabled');
+					// 0.25
+					if(small_value >= 0.1 && small_value <= 0.3){
+						$(this).find('li.active-whole').next('li').addClass('h_25')
+					}
+					// 0.5
+					if(small_value > 0.3 && small_value <= 0.55){
+						$(this).find('li.active-whole').next('li').addClass('h_5')
+					}
+					// 0.75
+					if(small_value > 0.55 && small_value < 1){
+						$(this).find('li.active-whole').next('li').addClass('h_75')
+					}
+				}
+				else{
+					$(this).find('li').addClass('disabled');
+				}
+			});
+		}
+		each_reiting($('.items-catalog ul.ul-reiting'))
+		// hover starts
+		$('ul.ul-reiting').hover(function() {
+			$(this).addClass('hover-reiting');
+		}, function() {
+			$(this).removeClass('hover-reiting');
+		});
+		$('ul.ul-reiting li').hover(function() {
+			$(this).addClass('active').prevAll('li').addClass('active');
+		}, function() {
+			$(this).removeClass('active').prevAll('li').removeClass('active');
+		});
+		// check localStorage
+		if(localStorage.getItem('user_id') != null){
+			var get_arr = localStorage.getItem('user_id').split(',');
+			for (var i = 0; i < get_arr.length; i++) {
+				$('.items-catalog .item[data-user-id="' + get_arr[i] + '"]').find('.ul-reiting').addClass('active')
+			}
+		}
+		// click reiting
+		$('ul.ul-reiting button').click(function(event) {
+			var has_active = $(this).parents('ul.ul-reiting').hasClass('active');
+			if(has_active == false ){
+				$(this).parents('ul.ul-reiting').addClass('active');
+				var th_reiting =+ $(this).attr('data-reiting');
+				console.log(th_reiting)
+				var id = $(this).parents('.item').attr('data-user-id');
+				if(localStorage.getItem('user_id') != null){
+					var get_item = localStorage.getItem('user_id');
+					localStorage.setItem('user_id', get_item + "," + id);
+				}
+				else{
+					localStorage.setItem('user_id', id);
+				}
+				// refresh reiting
+				for (var i = 0; i < json.length; i++) {
+					if(json[i].user_id == id){
+						var formula = 0;
+						var count_user = 0;
+						for (var key in json[i].reiting){
+							count_user = count_user + json[i].reiting[key];
+							var reiting =+ key;
+							formula = reiting * json[i].reiting[key] + formula;
+						}
+						var results = (formula + th_reiting) / (count_user + 1);
+						results = results.toFixed(2);
+						console.log(results)
+						$(this).parents('.ul-reiting').attr('data-reiting', results);
+						// add reiting
+						each_reiting($(this).parents('ul.ul-reiting'));
+
+						status_modal.fun_success("reiting");
+						return false;
+					}
+				}
+				
+			}
+			else{
+				status_modal.fun_er("reiting");
+			}
+		});
+	}
+}
+
 $(function () {
 	$.ajax({
-		url: "catalog.json"
+		url: "../catalog.json"
 	}).done(function (data) {
 		var th =  data.events;
 		for (var i = 0; i < th.length; i++) {
@@ -186,7 +303,17 @@ $(function () {
 				}
 			}
 			/*append in ul-list*/
-			$('.items-catalog').append(`<article class="item">
+			var reiting_li = {
+				fun: function(){
+					var result;
+					for (var i = 0; i < 5; i++) {
+						if(result == undefined) result = "";
+						result = result + `<li><button data-reiting="${i + 1}"></button></li>`;
+					}
+					return result;
+				}
+			}
+			$('.items-catalog').append(`<article class="item" data-user-id="${th[i].user_id}">
 				<div class="wr-content">
 					<div class="title-block">
 						<div class="img"><img src="${th[i].img_user}" alt=""></div>
@@ -215,8 +342,8 @@ $(function () {
 						</div>
 						<div class="reiting-block">
 							Средняя оценка:
-							<ul class="ul-reiting" data-reiting="${th[i].reiting}">
-								<li></li><li></li><li></li><li></li><li class="disabled"></li>
+							<ul class="ul-reiting">
+								${reiting_li.fun()}
 							</ul>
 						</div>
 						<div class="sharing">
@@ -245,6 +372,8 @@ $(function () {
 		fillter_catalog.fun(th);
 		// show-show-more catalog
 		show_catalog.fun_2();
+		// reiting
+		reiting_fun.fun(th);
 	});
 });
 
--- js/plugins/query-ui-include.js
+++ js/plugins/query-ui-include.js
@@ -1,3 +1,232 @@
 jQuery(document).ready(function($) {
 	$( ".select" ).selectmenu();
+
+
+
+
+
+
+
+
+
+
+
+
+
+	/*
+		<link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+		<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+		// less
+		body{
+			.custom-combobox {
+				position: relative;
+				display: block;
+				width: 100%;
+			}
+			.custom-combobox-toggle {
+				display: none;
+			}
+			.custom-combobox-input {
+				margin: 0;
+				padding: 5px 10px;
+			}
+			.ui-widget.ui-widget-content{
+				max-height: 300px;
+				overflow-x: hidden;
+				overflow-y: auto;
+			}
+		}
+		<span class="ui-widget">
+			<select id="combobox" class="combobox">
+				<option>option 1</option>
+				<option>option 2</option>
+				<option>option 3</option>
+				<option>option 4</option>
+				<option>option 5</option>
+				<option>option 6</option>
+				<option>option 7</option>
+				<option>option 8</option>
+				<option>option 9</option>
+				<option>Лохов Лох Лохович</option>
+			</select>
+		</span>
+	*/
+	jQuery(document).ready(function($) {
+		$( function() {
+			$.widget( "custom.combobox", {
+				_create: function() {
+					this.wrapper = $( "<span>" )
+						.addClass( "custom-combobox" )
+						.insertAfter( this.element );
+	 
+					this.element.hide();
+					this._createAutocomplete();
+					this._createShowAllButton();
+				},
+	 
+				_createAutocomplete: function() {
+					var selected = this.element.children( ":selected" ),
+						value = selected.val() ? selected.text() : "";
+	 
+					this.input = $( "<input>" )
+						.appendTo( this.wrapper )
+						.val( value )
+						.attr( "title", "" )
+						.addClass( "custom-combobox-input ui-widget ui-widget-content ui-state-default ui-corner-left" )
+						.autocomplete({
+							delay: 0,
+							minLength: 0,
+							source: $.proxy( this, "_source" ),
+							autoFocus: true,
+							// Додавання і видалення активного класу
+							open: function( event, ui ) {
+								$(this).parents('.custom-combobox').addClass('open-select');
+							},
+							close: function( event, ui ) {
+								$(this).parents('.custom-combobox').removeClass('open-select');
+							}
+	
+						})
+						.tooltip({
+							classes: {
+								"ui-tooltip": "ui-state-highlight"
+							}
+						});
+	 
+					this._on( this.input, {
+						autocompleteselect: function( event, ui ) {
+							ui.item.option.selected = true;
+							this._trigger( "select", event, {
+								item: ui.item.option
+							});
+	
+						},
+	 
+						autocompletechange: "_removeIfInvalid"
+					});
+				},
+	 
+				_createShowAllButton: function() {
+					var input = this.input,
+						wasOpen = false;
+	 
+					$( "<a>" )
+						.attr( "tabIndex", -1 )
+					 
+						.tooltip()
+						.appendTo( this.wrapper )
+						.button({
+							icons: {
+								primary: "ui-icon-triangle-1-s"
+							},
+							text: false
+						})
+						.removeClass( "ui-corner-all" )
+						.addClass( "custom-combobox-toggle ui-corner-right" )
+						.on( "mousedown", function() {
+							wasOpen = input.autocomplete( "widget" ).is( ":visible" );
+						})
+						.on( "click", function() {
+							input.trigger( "focus" );
+							// Close if already visible
+							if ( wasOpen ) {
+								return;
+							}
+							// Pass empty string as value to search for, displaying all results
+							input.autocomplete( "search", "" );
+						});
+				},
+				_source: function( request, response ) {
+					var matcher = new RegExp( $.ui.autocomplete.escapeRegex(request.term), "i" );
+					response( this.element.children( "option" ).map(function() {
+						var text = $( this ).text();
+						if ( this.value && ( !request.term || matcher.test(text) ) )
+							return {
+								label: text,
+								value: text,
+								option: this,
+							};
+					}) );
+				},
+				_removeIfInvalid: function( event, ui ) {
+					// Selected an item, nothing to do
+					if ( ui.item ) {
+						return;
+					}
+					// Search for a match (case-insensitive)
+					var value = this.input.val(),
+						valueLowerCase = value.toLowerCase(),
+						valid = false;
+					this.element.children( "option" ).each(function() {
+						if ( $( this ).text().toLowerCase() === valueLowerCase ) {
+							this.selected = valid = true;
+							return false;
+						}
+					});
+					// Found a match, nothing to do
+					if ( valid ) {
+						return;
+					}
+					// Remove invalid value
+					this.input
+						.val( "" )
+						
+						.tooltip( "open" );
+					this.element.val( "" );
+					this._delay(function() {
+						this.input.tooltip( "close" ).attr( "title", "" );
+					}, 2500 );
+					this.input.autocomplete( "instance" ).term = "";
+				},
+				_destroy: function() {
+					this.wrapper.remove();
+					this.element.show();
+				}
+			});
+			$( ".combobox" ).combobox();
+		} );
+	
+		$(window).load(function() {
+			// Додавання placeholder для input
+			$('.custom-combobox-input').each(function(index, el) {
+				var th_th_val = $(this).parents('.ui-widget').find('select option').first().val();
+				$(this).attr('placeholder', th_th_val);
+				$(this).val('');
+			});
+			// записування значення в input уже вибраного option
+			$('.ui-widget').each(function(index, el) {
+				var th_val = $(this).find('select option:selected').val();
+				$(this).find('input.custom-combobox-input').val(th_val);
+			});
+			// очищення input при його натисканні
+			$('.custom-combobox-input').click(function(event) {
+				$(this).parents('.ui-widget').find('.custom-combobox-toggle').trigger('click');
+				$(this).val('')
+			});
+			// при покиданні input перевіряти брати значення з option:selected (Захист від дурня)
+			$('.custom-combobox-input').focusout(function(event) {
+				 var th_val =  $(this).parents('.ui-widget').find('select option:selected').val();
+				$(this).val(th_val);
+			});
+		});
+	});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 });
